52d5e753e2102a02aee906bbfb568df4d8e32faf
stephan.fuchs <stephan.fuchs@robotron.de>
2024-08-16 13:55:26 +0200

    Glasstable Anpassungen für Drilldown

--- Saegewerk/appserver/static/glasstable.js
+++ Saegewerk/appserver/static/glasstable.js
@@ -1,12 +1,8 @@
 require(["splunkjs/mvc/searchmanager","splunkjs/mvc","jquery","splunkjs/mvc/simplexml/ready!"], function(SearchManager,mvc,$) {
 
-
-	console.log("Jetzt gehts los"); 
-
-
 // Color the circles and return the absolute value
 function einfarben(ID,wert){
-	wert=parseInt(wert)
+	wert=parseInt(wert) // Aus Eingabewert eine Intergerzahl machen
 	$(ID).html(wert);
 
 	if (wert>30){
@@ -24,33 +20,33 @@ function einfarben(ID,wert){
 
 // on click on chart, set token
 $("#saege1").on("click", function() {
-  	console.log("Auf Zahl saegewerk geklickt: "+$( this ).text() ); 
+  	console.log("Auf Säge Alpha geklickt: "+$( this ).text() ); 
 	var submittedTokens = mvc.Components.getInstance('submitted'); 
-	submittedTokens.set('token_chart_maschine', "1"); 
+	submittedTokens.set('token_chart_maschine', "Alpha"); 
 	submittedTokens.unset('token_chart_lager'); 
 });
 
 
 // on click on chart, set token
 $("#saege2").on("click", function() {
-	console.log("Auf Zahl saegewerk geklickt: "+$( this ).text() ); 
+	console.log("Auf Säge Beta geklickt: "+$( this ).text() ); 
   var submittedTokens = mvc.Components.getInstance('submitted'); 
-  submittedTokens.set('token_chart_maschine', "1"); 
+  submittedTokens.set('token_chart_maschine', "Beta"); 
   submittedTokens.unset('token_chart_lager'); 
 });
 
 
 // on click on chart, set token
 $("#saege3").on("click", function() {
-	console.log("Auf Zahl saegewerk geklickt: "+$( this ).text() ); 
+	console.log("Auf Säge Gamma geklickt: "+$( this ).text() ); 
   var submittedTokens = mvc.Components.getInstance('submitted'); 
-  submittedTokens.set('token_chart_maschine', "1"); 
+  submittedTokens.set('token_chart_maschine', "Gamma"); 
   submittedTokens.unset('token_chart_lager'); 
 });
 
-
+// on click on chart, set token
 $("#lagerzahl").on("click", function() {
-	console.log("Auf Zahl saegewerk geklickt: "+$( this ).text() ); 
+	console.log("Auf Lagerzahl geklickt: "+$( this ).text() ); 
   var submittedTokens = mvc.Components.getInstance('submitted'); 
   submittedTokens.set('token_chart_lager', "1"); 
   submittedTokens.unset('token_chart_maschine'); 
@@ -63,7 +59,7 @@ $("#lagerzahl").on("click", function() {
 var standort_search = new SearchManager({
 id: "standort_search_ID",
 search: 'index=prozesskette| stats latest(Auftragsnummer) as Auftragsnummer by Prozessschritt| join Auftragsnummer [search index=auftrag | table Kunde, Holz, Produkt, Gewicht, AuftragsID, time |  stats values(*) as * by AuftragsID| rename AuftragsID as Auftragsnummer]| join Auftragsnummer [search index=auftrag |  stats sum(Umsatz) as Umsatz, sum(Gewicht) as Gewicht by AuftragsID| rename AuftragsID as Auftragsnummer]| table Prozessschritt,Kunde, Holz, Produkt, Gewicht, AuftragsID, time',
-earliest_time:"-3m",
+earliest_time:"-10m",
 latest_time:"now"
 });
 
@@ -71,17 +67,14 @@ latest_time:"now"
 var lager_search = new SearchManager({
 	id: "search_lageranzahl",
 	search: 'index="prozesskette" Prozessschritt=L* | table _time Prozessschritt Lager| sort  _time| stats latest(*) as * by Prozessschritt|  addcoltotals label=Summe labelfield=Prozessschritt',
-	earliest_time:"-3m",
+	earliest_time:"-10m",
 	latest_time:"now"
 });
 
 
 var standort_results = standort_search.data("preview");
-
 standort_results.on("data", function() {
-
 	console.log("Data (rows) 0: ", standort_results.data());
-
 	// Holz für Sägen
 	aktueller_stand_saege1=standort_results.data().rows[0][3]; // ALpha
 	aktueller_stand_saege2=standort_results.data().rows[1][3]; // Beta
@@ -101,10 +94,6 @@ standort_results.on("data", function() {
 	einfarben("#gewicht3",aktueller_stand_gewicht3)
 	einfarben("#gewicht4",aktueller_stand_gewicht4)
 	einfarben("#gewicht5",aktueller_stand_gewicht5)
-
-
-
-
 });
 
 var lager_results = lager_search.data("preview");
